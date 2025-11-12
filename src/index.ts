59f2f72083be633a217bc1bd672541f9d0ad6512
bemunin <tw.munin@gmail.com>
2025-11-12 18:46:18 +0700

    Add type schema to vision_msgs/msg/Detection2DArray

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ type MyGps = {
 export function activate(extensionContext: ExtensionContext): void {
   extensionContext.registerPanel({ name: "example-panel", initPanel: initExamplePanel });
   extensionContext.registerMessageConverter({
+      type: "schema",
       fromSchemaName: "vision_msgs/msg/Detection2DArray",
       toSchemaName: "foxglove.ImageAnnotations",
       converter: (_msg: MyGps): Partial<ImageAnnotations> => {

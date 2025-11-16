176f8353a01b1907048cab4ea378c25c690a6e4b
bemunin <tw.munin@gmail.com>
2025-11-16 18:25:55 +0700

    Fix code format

--- src/detection2dArray/types.ts
+++ src/detection2dArray/types.ts
@@ -3,42 +3,40 @@ import { Pose, Vector2 } from "@foxglove/schemas";
 export type PoseWithCovariance = {
   covariance: number[]; // length 36
   pose: Pose;
-}
+};
 
-export type ObjectHypothesisWithPose={
+export type ObjectHypothesisWithPose = {
   hypothesis: {
-    class_id: string,
-    score: number, // [0-1]
+    class_id: string;
+    score: number; // [0-1]
   };
   pose: PoseWithCovariance;
+};
 
-}
-
-
-export type Pose2D ={
+export type Pose2D = {
   position: Vector2;
   theta: number;
-}
+};
 
 export type BoundingBox2D = {
   center: Pose2D;
   size_x: number;
   size_y: number;
-}
+};
 
 export type Detection2D = {
   header: Header;
   results: ObjectHypothesisWithPose[];
   bbox: BoundingBox2D;
   id: string;
-}
+};
 
 export type Header = {
   stamp: { sec: number; nanosec: number };
   frame_id: string;
-}
+};
 
 export type Detection2DArray = {
   header: Header;
   detections: Detection2D[];
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@ import { ExtensionContext } from "@foxglove/extension";
 // import { initExamplePanel } from "./examples/ExamplePanel";
 import { detection2DArrayConverter } from "./detection2dArray/converter";
 
-
 export function activate(extensionContext: ExtensionContext): void {
   // extensionContext.registerPanel({ name: "example-panel", initPanel: initExamplePanel });
   extensionContext.registerMessageConverter({
-      type: "schema",
-      fromSchemaName: "vision_msgs/msg/Detection2DArray",
-      toSchemaName: "foxglove.ImageAnnotations",
-      converter: detection2DArrayConverter,
+    type: "schema",
+    fromSchemaName: "vision_msgs/msg/Detection2DArray",
+    toSchemaName: "foxglove.ImageAnnotations",
+    converter: detection2DArrayConverter,
   });
 }

3fb94d4f71a270c4214631988d76c2e32302343f
bemunin <tw.munin@gmail.com>
2025-12-14 12:12:06 +0700

    feat(cli): add cli package boilerplate

---
+++ packages/cli/README.md
@@ -0,0 +1,1 @@
+# 👋 Isaac Powerpack Commandline
---
+++ packages/cli/pow/main.py
@@ -0,0 +1,11 @@
+import click
+
+
+@click.group()
+def pow():
+    """Isaac Powerpack CLI"""
+    click.echo("TODO: Implement Isaac Powerpack CLI commands")
+
+
+if __name__ == "__main__":
+    pow()
